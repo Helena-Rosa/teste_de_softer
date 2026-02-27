cbe484692966b8ab9a5506532f823bee72b53631
Helena Rosa <helena.rosa@aluno.senai.br>
2026-02-27 11:28:46 -0300

    ADD: testando os erros

---
+++ teste/test_verificador_media_erros.py
@@ -0,0 +1,6 @@
+import pytest 
+from escola import verificador_media
+
+def test_string_como_entrada():
+    with pytest .raises(TypeError, match = "Tipo invalido, a entrada deve ser numerica"):
+        verificador_media("CASA")
