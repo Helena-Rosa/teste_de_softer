e570918f7310070695464013c1efb382278276ed
Helena Rosa <helena.rosa@aluno.senai.br>
2026-03-06 17:29:37 -0300

    corrigindo erros

--- escola.py
+++ escola.py
@@ -2,15 +2,30 @@ def verificador_media (media: int|float) -> str:
     """Esta função retorna se o aluno passou ano"""
 
 
+    
+    if isinstance(media, int):
+        raise TypeError("Tipo invalido, a entrada deve ser numerica")
+    
+
+
+
     if media >= 7:
         return "Aprovado"
     
-    elif media < 5:
+    elif media < 4:
         return "Reprovado"
     
     else:
-        return "Recuperação"
+        raise ValueError("Entrada Invalida")
     
+
+
+    
+
+
+
+
+
 
 if __name__ == "__main__":
     print(verificador_media(7))
--- teste/test_verificador_media_erros.py
+++ teste/test_verificador_media_erros.py
@@ -3,4 +3,18 @@ from escola import verificador_media
 
 def test_string_como_entrada():
     with pytest .raises(TypeError, match = "Tipo invalido, a entrada deve ser numerica"):
-        verificador_media("CASA")
+        verificador_media("OITO")
+
+
+def test_menor_que_dez_como_entrada():
+    with pytest .raises(TypeError, match = "O valor é maior que 0 e menor a 10"):
+        verificador_media("-5")
+
+
+
+def test_maior_que_dez_como_entrada():
+    with pytest .raises(TypeError, match = "O valor é maior que 0 e menor a 10"):
+        verificador_media("2000")
+
+
+
